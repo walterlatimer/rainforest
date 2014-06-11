13de617dfe2d02e18eca5ecdbde7ae59c7f25058
Walter Latimer, Jr <walter@walterlatimer.com>
2014-06-10 22:06:54 -0400

    Added validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ActiveRecord::Base
+	before_save :default_values
 	has_many :reviews
+
+	validates :username, uniqueness: true,
+                       presence: true,
+                       format: { with: /\A(?=.*[a-z])[a-z\d]+\Z/i },
+                       length: { in: 3..15 }
+	validates :password, presence: true,
+                       confirmation: true,
+                       length: { in: 6..20 }
+	validates :display,  presence: true,
+	                     length: { maximum: 20 }
+	# TODO: Validate avatar URL
+	validates :access,   numericality: {
+		                     only_integer: true,
+		                     greater_than: 0,
+		                     less_than_or_equal_to: 4
+		                   }
+
+	private
+	# Access level is 0 by default
+  def default_values
+    self.access ||= 1
+  end
 end
