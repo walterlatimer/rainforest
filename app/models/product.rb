4d2d865627eb0ca6a777627fb6d837390dbc0cc9
Walter Latimer, Jr <walter@walterlatimer.com>
2014-06-10 22:57:11 -0400

    Added Product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,28 @@
 class Product < ActiveRecord::Base
-  belongs_to :category
-  has_many :photos, :reviews
+	before_save :default_values
+	belongs_to :category
+	# has_many :photos, :reviews
+
+	validates :name,           presence: true
+	validates :price_in_cents, numericality: {
+		                           only_integer: true,
+		                           greater_than: 0
+		                         }
+	validates :quantity,       numericality: {
+                               only_integer: true,
+                               greater_than_or_equal_to: 0
+	                           }
+	validates :description,    presence: true
+	validates :category,       presence: true
+
+	private
+	def default_values
+		self.featured ||= false
+	end
+
+	def formatted_price
+		price_in_dollars = price_in_cents.to_f / 100
+		sprintf("%.2f", price_in_dollars)
+	end
+
 end
