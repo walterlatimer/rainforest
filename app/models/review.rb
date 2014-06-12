e798311af5219a5be08ba9584745f9d4845aa9e7
Bryan <bryan@webphenoms.com>
2014-06-12 11:02:25 -0400

    reviews updates

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,13 @@
 class Review < ActiveRecord::Base
-  belongs_to :product, :user
+  belongs_to :product
+  belongs_to :user
+  
+  validates :title, presence: true, length: { minimum: 2 }
+  
+  validates :comment, presence: true, length: { minimum: 5}
+
+  validates :rating, numericality: { only_integer: true }
+
+
+
 end
